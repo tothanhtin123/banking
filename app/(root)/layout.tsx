b5b9258aca413e50a06895eed41e65a9925cbfeb
tothanhtin123 <tothanhtin123@gmail.com>
2024-07-22 21:19:21 +0700

    feat: init project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import React, { ReactNode } from "react";
+
+const RootLayout = ({children}:{children:ReactNode}) => {
+  return <main>
+    SIDEBAR
+    {children}
+  </main>
+};
+
+export default RootLayout;
