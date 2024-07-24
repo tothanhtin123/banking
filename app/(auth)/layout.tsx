93a3f7fedd8e560b2e351ba36e33eb8dfc3c5abf
tothanhtin123 <tothanhtin123@gmail.com>
2024-07-24 22:43:57 +0700

    feat: layout for auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
 import React, { ReactNode } from "react";
 
-const AuthLayout = ({children}:{children:ReactNode}) => {
-  return <main>
-    {children}
-  </main>
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return <main>{children}</main>;
 };
 
 export default AuthLayout;
