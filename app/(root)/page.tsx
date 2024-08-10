5c95403b3de25490a06765b9b7cfc931698a0d39
tothanhtin123 <tothanhtin123@gmail.com>
2024-08-10 10:12:20 +0700

    feat: show current transaction data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,25 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = async ({searchParams:{id, page}}:SearchParamProps) => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({
-    userId:loggedIn.$id,
+    userId: loggedIn.$id,
+  });
 
-  })
-
-  if(!accounts) return;
+  if (!accounts) return;
 
   const accountsData = accounts?.data;
 
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  const account = await getAccount({appwriteItemId});
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
@@ -36,12 +37,17 @@ const Home = async ({searchParams:{id, page}}:SearchParamProps) => {
             totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        RECENT TRANSACTIONS
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
       <RightSidebar
         user={loggedIn}
         transactions={accounts?.transactions}
-        banks={accountsData?.slice(0,2)}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   );
